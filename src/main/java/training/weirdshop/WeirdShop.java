ff7d7d2f7c1d8c870aaa934062dafa25dcbd9646
James Griffiths <james.griffiths@softwire.com>
2019-01-23 10:59:04 +0000

    Create WeirdShop - code and regression test

---
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -0,0 +1,62 @@
+package training.weirdshop;
+
+class WeirdShop {
+    private Item[] items;
+
+    public WeirdShop(Item[] items) {
+        this.items = items;
+    }
+
+    void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals("Aged Brie")
+                    && !items[i].name.equals("Backstage Pass")) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals("Gold Coin")) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals("Backstage Pass")) {
+                        if (items[i].sellIn < 12) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 7) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!items[i].name.equals("Gold Coin")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals("Aged Brie")) {
+                    if (!items[i].name.equals("Backstage Pass")) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals("Gold Coin")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/training/weirdshop/RegressionTest.java
@@ -0,0 +1,42 @@
+package training.weirdshop;
+
+import org.junit.Test;
+import java.util.Scanner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RegressionTest {
+    @Test
+    public void itemsChangeInTheSameWayTheyDidBeforeWeMadeAnyChanges() {
+        Item[] items = {
+                new Item("Aged Brie", 40, 17),
+                new Item ("Backstage Pass", 20, 1),
+                new Item ("Gold Coin", 5, 80),
+                new Item ("Some other item", 5, 17)
+        };
+
+        StringBuilder sb = new StringBuilder();
+        WeirdShop weirdShop = new WeirdShop(items);
+
+        addItems(sb, 0, items);
+
+        for (int day = 1; day <= 50; day++) {
+            weirdShop.updateQuality();
+            addItems(sb, day, items);
+        }
+
+        String output = sb.toString();
+
+        String expected =  new Scanner(getClass().getResourceAsStream("/expected-output.txt"), "UTF-8").useDelimiter("\\A").next();
+
+        assertThat(output).isEqualTo(expected);
+    }
+
+    private void addItems(StringBuilder sb, int step, Item[] items) {
+        sb.append(String.format("\nDay %d:\n", step));
+
+        for (Item item : items)
+        {
+            sb.append(String.format("  %s: Quality %d, Sell In %d\n", item.name, item.quality, item.sellIn));
+        }
+    }
+}
